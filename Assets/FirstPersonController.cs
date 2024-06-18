83793af51a1c17d6abb3b633004d20c36b11c835
M-Nabeel-Aslam <m.nabeel4468@gmail.com>
2024-06-18 23:15:34 +0500

    Initial commit

---
+++ Assets/FirstPersonController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FirstPersonController : MonoBehaviour
+{
+   
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.LogError("OnTriggerEnter");
+        if (collision.transform.tag == "Items")
+        {
+            
+            Debug.LogError("OnTriggerEnter Items");
+        }
+
+    }
+
+
+    public void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        Debug.LogError("OnCollisionEnter");
+        if (collision.transform.tag == "Items")
+        {
+           
+            Debug.LogError("OnCollisionEnter Items");
+ 
+        }
+    }
+
+
+}
+
